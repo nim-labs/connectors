37a2ecb60250ab42f299740010b4ed9a0feb54ae
Andrew Sinagra <andrew@nim-labs.com>
2017-05-03 20:52:10 -0700

    updating version number in nim_c4d

--- nim_core/nim_c4d.py
+++ nim_core/nim_c4d.py
@@ -28,7 +28,7 @@ import nim_print as P
 import nim_win as Win
 
 #  Variables :
-version='v2.5.0'
+version='v2.6.0'
 winTitle='NIM_'+version+' - '
 _os=platform.system().lower()
 nim_plugin_ID=1032427
