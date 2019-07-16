610778d80ab91717d3d4a3d3945fd1ae6a325c3a
Andrew Sinagra <andrew@nim-labs.com>
2019-07-16 14:29:56 -0700

    Updating version in python files

--- nim_core/UI.py
+++ nim_core/UI.py
@@ -2,7 +2,7 @@
 #******************************************************************************
 #
 # Filename: UI.py
-# Version:  v4.0.27.190418
+# Version:  v4.0.32.190716
 #
 # Copyright (c) 2014-2019 NIM Labs LLC
 # All rights reserved.
@@ -43,7 +43,7 @@ except ImportError :
 #  Variables :
 WIN=''
 startTime=''
-version='v4.0.27'
+version='v4.0.32'
 winTitle='NIM_'+version
 _os=platform.system().lower()
 _osCap=platform.system()
--- nim_core/nim_c4d.py
+++ nim_core/nim_c4d.py
@@ -2,7 +2,7 @@
 #******************************************************************************
 #
 # Filename: nim_c4d.py
-# Version:  v4.0.27.190418
+# Version:  v4.0.32.190716
 #
 # Copyright (c) 2014-2019 NIM Labs LLC
 # All rights reserved.
@@ -28,7 +28,7 @@ import nim_print as P
 import nim_win as Win
 
 #  Variables :
-version='v4.0.27'
+version='v4.0.32'
 winTitle='NIM_'+version+' - '
 _os=platform.system().lower()
 nim_plugin_ID=1032427
