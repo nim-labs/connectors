6c51bd4c873886a82a096ed1733bca8228c541bb
Andrew Sinagra <andrew@nim-labs.com>
2020-03-13 19:41:17 -0700

    renaming python folders

--- nim_core/py3/nim_api.py
+++ nim_core/py3/nim_api.py
@@ -2,7 +2,7 @@
 #******************************************************************************
 #
 # Filename: nim_api.py
-# Version:  v4.0.47.200224
+# Version:  v4.0.48.200313
 #
 # Copyright (c) 2014-2020 NIM Labs LLC
 # All rights reserved.
