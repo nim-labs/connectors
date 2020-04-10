07215ea66a952827dbe3eee3ef78fd2a6e651209
Andrew Sinagra <andrew@nim-labs.com>
2020-04-10 15:51:00 -0700

    Adding nim_core files to determine path options for Python3 vs Python2

---
+++ nim_core/nim_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#******************************************************************************
+#
+# Filename: nim_core/nim_file.py
+# Version:  v4.0.49.200410
+#
+# Copyright (c) 2014-2020 NIM Labs LLC
+# All rights reserved.
+#
+# Use of this software is subject to the terms of the NIM Labs license
+# agreement provided at the time of installation or download, or which
+# otherwise accompanies this software in either electronic or hard copy form.
+# *****************************************************************************
+
+import os,sys
+
+pythonVersion = sys.version_info.major
+
+if pythonVersion == 3 :
+    # Import nim_core for Python3
+    from py3.nim_file import *
+
+else :
+    # Import nim_core for Python2
+    from py2.nim_file import *
