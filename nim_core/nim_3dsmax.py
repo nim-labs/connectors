fd61a26451b19071fecef8414e64e820b52d08c6
Andrew Sinagra <andrew@nim-labs.com>
2021-01-04 16:33:09 -0800

    Updating nim_core version and copyright

--- nim_core/nim_3dsmax.py
+++ nim_core/nim_3dsmax.py
@@ -2,9 +2,9 @@
 #******************************************************************************
 #
 # Filename: nim_3dsmax.py
-# Version:  v4.0.60.201223
+# Version:  v4.0.61.210104
 #
-# Copyright (c) 2014-2020 NIM Labs LLC
+# Copyright (c) 2014-2021 NIM Labs LLC
 # All rights reserved.
 #
 # Use of this software is subject to the terms of the NIM Labs license
@@ -27,7 +27,7 @@ except :
     except : pass
 
 #  Variables :
-version='v4.0.60'
+version='v4.0.61'
 winTitle='NIM_'+version
 
 def get_mainWin() :
