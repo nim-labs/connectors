548b262ae62ae5a1a3c54daa090649cbfd84fb12
Andrew Sinagra <andrew@nim-labs.com>
2021-09-22 13:00:40 -0700

    Updating Version

--- nim_core/py3/nim_flame.py
+++ nim_core/py3/nim_flame.py
@@ -2,7 +2,7 @@
 #******************************************************************************
 #
 # Filename: nim_flame.py
-# Version:  v5.0.14.210914
+# Version:  v5.0.15.210922
 #
 # Copyright (c) 2014-2021 NIM Labs LLC
 # All rights reserved.
@@ -37,7 +37,7 @@ except ImportError :
 				print("NIM: Failed to load UI Modules - Flame")
 
 #  Variables :
-version='v5.0.14'
+version='v5.0.15'
 winTitle='NIM_'+version
 
 
